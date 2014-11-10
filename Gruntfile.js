81ef3d290a6fb463b1fb1928cfc73bb9986c8fb8
Sudharshan <sudhar.ti@gmail.com>
2014-11-10 01:42:56 -0600

    Add everything, define a structure

---
+++ Gruntfile.js
@@ -0,0 +1,105 @@
+/*global module:false*/
+module.exports = function(grunt) {
+  'use strict';
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    bower_conf: grunt.file.exists('.bowerrc') ? grunt.file.readJSON('.bowerrc') : { directory : 'bower_components' },
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> \n' +
+    '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+    '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+    ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+
+    // Task configuration.
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: false
+      },
+      dist: {
+        src: ['src/javascripts/*.js'],
+        dest: 'dist/js/<%= pkg.name %>.js'
+      }
+    },
+    
+    copy: {
+
+    },
+
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      dist: {
+        src: '<%= concat.dist.dest %>',
+        dest: 'dist/js/<%= pkg.name %>.min.js'
+      }
+    },
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        browser: true,
+        globals: {}
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib_test: {
+        src: ['lib/**/*.js', 'test/**/*.js']
+      }
+    },
+    qunit: {
+      files: ['test/**/*.html']
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      lib_test: {
+        files: '<%= jshint.lib_test.src %>',
+        tasks: ['jshint:lib_test', 'qunit']
+      }
+    },
+    sass: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '<%= bower_conf.directory %>/bootstrap-sass-official/assets/stylesheets/',
+          src: ['bootstrap.scss'],
+          dest: 'dist/css/vendor/',
+          ext: '.css'
+        }]
+      }
+    },
+    connect: {
+      base: {
+        options: {
+          port: 4200,
+          open: true,
+          keepalive: true
+        }
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  require('load-grunt-tasks')(grunt);
+
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify', 'connect']);
+
+};
---
+++ dist/js/bs-social-feed.js
@@ -0,0 +1,2 @@
+/*! bs-social-feed - v0.0.1 
+* Copyright (c) 2014 "Sudharsanan" Muralidharan; Licensed  */
