ac7569d98b3d8f90f762d325c3695de2c1f3c079
Sudharshan <sudhar.ti@gmail.com>
2014-11-13 01:03:40 -0600

    Style newsfeed, have to separate reusable components

--- demo/js/demo.js
+++ demo/js/demo.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
   $.backstretch('../../img/background-2.jpg');
   $('.cover-pic').backstretch('../../img/cover-pic.jpg');
+
+  $('#new-micropost textarea').autosize();
+
 });
