a694b2ecaaac1ed5e41cc84e9469201146df2141
Sudharshan <sudhar.ti@gmail.com>
2014-11-24 22:47:04 -0600

    Started to dev timeline template

--- templates/js/demo.js
+++ templates/js/demo.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $.backstretch('../img/background-2.jpg', {speed: 1000});
-  $('.cover-pic').backstretch('../img/cover-pic.jpg');
+  $('.cover-pic').backstretch('../img/img-1-small.jpg');
 
   $('#new-micropost textarea').autosize();
 
