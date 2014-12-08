ea12f8a6d7d7bb2b7b1b2be27f9fc35fc9481608
Sudharshan <sudhar.ti@gmail.com>
2014-12-08 03:28:19 -0600

    Change image names

--- templates/js/demo.js
+++ templates/js/demo.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $.backstretch('../img/background-2.jpg', {speed: 1000});
-  $('.cover-pic').backstretch('../img/img-1-small.jpg');
+  $('.cover-pic').backstretch('../img/img-1.jpg');
 
   $('#new-micropost textarea').autosize();
 
