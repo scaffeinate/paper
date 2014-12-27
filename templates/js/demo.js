555ebb8451eddbd92956cedb6e81ee4bc45a37ec
Sudharshan <sudhar.ti@gmail.com>
2014-12-27 04:02:04 -0600

    Changes to timeline template, cleanup code

--- templates/js/demo.js
+++ templates/js/demo.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $.backstretch('../assets/img/background-2.jpg', {speed: 1000});
+  $.backstretch('../assets/img/background-3.jpg', {speed: 1000});
   $('.cover-pic').backstretch('../assets/img/img-1.jpg');
 
   $('#new-micropost textarea').autosize();
