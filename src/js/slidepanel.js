59b9194161acfdde7f8c5f230a9b9417aeb2c171
Sudharshan <sudhar.ti@gmail.com>
2014-12-29 04:55:08 -0600

    Slidepanel plugin in progress

--- src/js/slidepanel.js
+++ src/js/slidepanel.js
@@ -1,28 +1,55 @@
 +function (global, $) {
   'use strict';
 
-  var position = 0;
+  var position = 0, z_index = 0;
 
   var SlidePanel = function(element, options) {
-    this.init();
+    this.init(element, options);
   };
 
   SlidePanel.DEFAULTS = {
-    overlayClassName: 'mdm-overlay',
+    overlayClass: 'mdm-overlay',
+    bodyClass: 'mdm-body',
+    overlayTemplate: '<div id="overlay" class="mdm-overlay"></div>'
   };
 
-  SlidePanel.prototype.init = function() {
+  SlidePanel.prototype.init = function(element, options) {
+    this.$element = $(element);
+    this.options = options;
+    var _this = this;
+    
     if(position == 0) {
+      $('body').append(this.options.overlayTemplate);
+      this.$overlay = $('#overlay');
+      this.$overlay.click(function() {
+        _this.slide();
+      });
       
+      if($('body').css('zIndex') == 'auto') {
+        z_index = 0;
+      } else {
+        z_index = $('body').css('zIndex');
+      }
+      
+      this.$element.css('zIndex', z_index+1);
+      this.$overlay.css('zIndex', z_index+1);
     }
   };
 
   SlidePanel.prototype.slide = function() {
-
+    
   };
 
-  var Plugin = function(){
-    var slide = new SlidePanel();
+  function Plugin() {
+    return this.each(function () {
+      var $this   = $(this);
+      var data    = $this.data('panel');
+      var options = $.extend({}, SlidePanel.DEFAULTS, $this.data());
+
+      if(data) {
+        new SlidePanel(this, options);
+      }
+    });
   };
 
   var old = $.fn.button;
